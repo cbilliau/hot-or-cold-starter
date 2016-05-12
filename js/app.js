27eaf054d3a58af5e8cb15f9d239367b751b8f40
cbilliau <christopher@billiau.com>
2016-05-12 14:39:10 -0400

    Add guess evaluation function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 
 $(document).ready(function(){
+'use strict'
+// Variables
+	var randomNum = "";
+	var userNum = "";
+	var evalNum = "";
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(event){
@@ -14,34 +19,48 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-	//Start game on load
+//Start game on load
 	newGame();
-
-	// Start a new game
+// Start a new game
 	$('.new').on('click', function() {
 		newGame();
 	});
-	// newGame function
+// newGame function
 	function newGame() {
 		genNum();
 		console.log(genNum());
 	}
-	// Random number generator
+// Random number generator
 	function genNum() {
-		var randomNum =  Math.floor((Math.random() *100)+1);
+		randomNum =  Math.floor((Math.random() *100)+1);
 		return randomNum;
 	}
-
-	// Take user input and evaluate
+// Take user input and evaluate
 	function userInput() {
 		$('.game').on('click', '#guessButton', function(event) {
 		event.preventDefault();
 		// get value input
 		var userNum = $('#userGuess').val();
-
 		// evaluate value (far,close, correct?)
 		// return feedback (cold, hot, correct?)
-
-	});
+		evalNum = evaluate(userNum, randomNum);
+		if (evalNum <= 20) {
+			$('#feedback').append('<h2>You\'re hot</h2>')
+		} else if (evalNum > 20) {
+			$('#feedback').append('<h2>You\'re cold</h2>')
+		}	else {
+			$('#feedback').append('<h2>Correct!</h2>')
+		}
 	}
+)}
+
+// Function to evaluate number
+	function evalNum(num1, num2) {
+		if (num1 > num2) {
+			return num1-num2;
+		} else {
+			return num2-num1;
+			}
+	}
+
 });
