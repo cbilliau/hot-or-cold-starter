61905a626d3351ec6ea33f3f9e16a7ffc3458f95
cbilliau <christopher@billiau.com>
2016-05-12 13:14:12 -0400

    Add func for game button, num generator, start on num eval

--- js/app.js
+++ js/app.js
@@ -1,17 +1,47 @@
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
+  	$(".what").click(function(event){
+    	event.preventDefault();
     	$(".overlay").fadeIn(1000);
 
   	});
 
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
+
   		$(".overlay").fadeOut(1000);
   	});
 
+	//Start game on load
+	newGame();
+
+	// Start a new game
+	$('.new').on('click', function() {
+		newGame();
+	});
+	// newGame function
+	function newGame() {
+		genNum();
+		console.log(genNum());
+	}
+	// Random number generator
+	function genNum() {
+		var randomNum =  Math.floor((Math.random() *100)+1);
+		return randomNum;
+	}
+
+	// Take user input and evaluate
+	function userInput() {
+		$('.game').on('click', '#guessButton', function(event) {
+		event.preventDefault();
+		// get value input
+		var userNum = $('#userGuess').val();
+
+		// evaluate value (far,close, correct?)
+		// return feedback (cold, hot, correct?)
+
+	});
+	}
 });
-
-
