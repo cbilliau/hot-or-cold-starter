b86ab22f3d3f17c8d1adb4704ea94618cc982cb2
cbilliau <christopher@billiau.com>
2016-05-12 22:42:21 -0400

    Add guess # tracker and list of guesses.

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
 	var randomNum = "";
 	var userNum = "";
 	var evalNum = "";
+	var counter = "0";
 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(event){
@@ -30,7 +31,9 @@ $(document).ready(function(){
 	function newGame() {
 		genNum();
 		userInput();
+		fieldReset();
 		console.log(randomNum + ' random # generated');
+
 	}
 // Random number generator
 	function genNum() {
@@ -42,12 +45,16 @@ $(document).ready(function(){
 		$('input#guessButton').click(function(event) {
 		event.preventDefault();
 		console.log('user input clicked')
+		// call counterAdd func to increase counter
+		counterAdd();
 		// get value input
 		var userNum = $('#userGuess').val();
 		$('#userGuess').val('');
 		// evaluate value (far,close, correct?)
 		// return feedback (cold, hot, correct?)
 		userNum = Math.abs(Math.floor(userNum));
+		// Send guess # to guess list
+		addGuess(userNum);
 		evalNum = evaluate(userNum, randomNum);
 		console.log('difference btwn #s: ' + evalNum);
 		if ((evalNum <= 20) && (userNum != randomNum)) {
@@ -66,6 +73,27 @@ $(document).ready(function(){
 		} else {
 			return num2-num1;
 			}
+	}
+// Function to track # of guesses user makes
+	// Start counter at 0 on load and for new game
+	function counterAdd() {
+		// When guess submitted add '1' to number in  span#count
+		counter++;
+		$('span#count').replaceWith('<span id="count">'+counter+'</span>');
+		console.log(counter);
+	}
+
+// Guess list
+	function addGuess(guessNum) {
+		$('.game ul').append('<li>' + guessNum + '</li>');
+	}
+
+// Function to reset game section
+	function fieldReset() {
+		// Reset counter
+		$('span#count').replaceWith('<span id="count">0</span>');
+		// Reset feedback
+		$('h2#feedback').replaceWith('<h2 id="feedback">Make your Guess!</h2>');
 	}
 
 });
