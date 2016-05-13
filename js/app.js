fbaad3b3c03f3268ca5c35814b0534928dc93a01
cbilliau <christopher@billiau.com>
2016-05-13 07:02:09 -0400

    Set critera for userInput. completed reset function.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
 'use strict'
+
 // Variables
 	var randomNum = "";
 	var userNum = "";
@@ -22,50 +23,53 @@ $(document).ready(function(){
 
 //Begin game on load
 	newGame();
-// Start a new game
+
+// Start new game w/ call function
 	$('.new').click( function() {
 		newGame();
-		console.log('start new game button clicked');
-	});
+	})
+
 // newGame function
 	function newGame() {
 		genNum();
 		userInput();
 		fieldReset();
-		console.log(randomNum + ' random # generated');
-
 	}
+
 // Random number generator
 	function genNum() {
 		randomNum =  Math.floor((Math.random() *100)+1);
+		console.log(randomNum);
 		return randomNum;
 	}
+
 // Take user input and evaluate
 	function userInput() {
 		$('input#guessButton').click(function(event) {
 		event.preventDefault();
-		console.log('user input clicked')
-		// call counterAdd func to increase counter
-		counterAdd();
-		// get value input
-		var userNum = $('#userGuess').val();
+		var userNum = $('#userGuess').val(); // get value input
 		$('#userGuess').val('');
-		// evaluate value (far,close, correct?)
-		// return feedback (cold, hot, correct?)
-		userNum = Math.abs(Math.floor(userNum));
-		// Send guess # to guess list
-		addGuess(userNum);
-		evalNum = evaluate(userNum, randomNum);
-		console.log('difference btwn #s: ' + evalNum);
-		if ((evalNum <= 20) && (userNum != randomNum)) {
+		userNum = Math.abs(Math.floor(userNum)); // Convert to abs integer.
+		evalNum = evaluate(userNum, randomNum); // evaluate value
+		if ( isNaN(userNum) || (userNum >=101) || (userNum == '')) { /*eval if NaN or > 100*/
+			$('#feedback').html('Try again.')
+		} else if ((evalNum <= 20) && (userNum != randomNum)) {
 			$('#feedback').html('You\'re hot')
+			addGuess(userNum); // Send guess # to guess list
+			counterAdd(); // call counterAdd func to increase counter
 		} else if (evalNum > 20) {
 			$('#feedback').html('You\'re cold')
+			addGuess(userNum); // Send guess # to guess list
+			counterAdd(); // call counterAdd func to increase counter
 		}	else {
 			$('#feedback').html('Correct!')
+			addGuess(userNum); // Send guess # to guess list
+			counterAdd(); // call counterAdd func to increase counter
+			// winBox(); // -in progress- call to box to restart game
 		}
 	}
 )}
+
 // Function to evaluate distance btwn numbers
 	function evaluate(num1, num2) {
 		if (num1 > num2) {
@@ -74,26 +78,29 @@ $(document).ready(function(){
 			return num2-num1;
 			}
 	}
+
 // Function to track # of guesses user makes
-	// Start counter at 0 on load and for new game
 	function counterAdd() {
-		// When guess submitted add '1' to number in  span#count
-		counter++;
+		counter++; // When guess submitted add '1' to number in  span#count
 		$('span#count').replaceWith('<span id="count">'+counter+'</span>');
-		console.log(counter);
 	}
 
-// Guess list
+// Guess list creator
 	function addGuess(guessNum) {
 		$('.game ul').append('<li>' + guessNum + '</li>');
 	}
 
+// Winner modal
+	// function winBox() {
+	// 	var r = confirm ("Winner. Play again?")
+	// 	if (r == true) {
+	// 		newGame();
+	// 	} else userInput();
+	// }
 // Function to reset game section
 	function fieldReset() {
-		// Reset counter
-		$('span#count').replaceWith('<span id="count">0</span>');
-		// Reset feedback
+		$('span#count').replaceWith('<span id="count">0</span>'); // Reset counter
 		$('h2#feedback').replaceWith('<h2 id="feedback">Make your Guess!</h2>');
+		$('.game li').remove(); //Reset feedback
 	}
-
 });
